5da47858ff805426f9a4d27ae48167a73bfe6c60
Eduardo <echavez@utea.edu.pe>
2024-01-16 11:55:09 -0500

    Ejercicio4 a medias

--- Ejercicios01/Ejercicio4.java
+++ Ejercicios01/Ejercicio4.java
@@ -2,6 +2,9 @@ package Ejercicios01;
 
 public class Ejercicio4 {
     public static void main(String[] args) {
-        
+      System.out.println("Lunes\t\tMartes\tMiercoles\tJueves\tViernes");  
+      System.out.println("-----\t\t------\t---------\t------\t-------");  
+      System.out.println("Alogoritmos 1\t\tAlgoritmos 1\t\t");  
+
     }
 }
