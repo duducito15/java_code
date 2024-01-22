6100135c6afa528f8cb17a8cc5e754f612d70a3a
Eduardo <echavez@utea.edu.pe>
2024-01-22 09:59:51 -0500

    Variables

---
+++ Ejercicios01/Ejercicio5.java
@@ -0,0 +1,12 @@
+package Ejercicios01;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+      System.out.println("  /\\_/\\  ");
+      System.out.println(" ( o.o ) ");
+      System.out.println("  > ^ <  ");
+      System.out.println(" /  |  \\ ");
+      System.out.println("/   |   \\");
+
+      }
+}
---
+++ Variables.java
@@ -0,0 +1,54 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Variables
+        /*
+         * Nombre de las variables, siemrpe empezar con una letra
+         * la nomenclatura lowerCamelCase, ejemplos:
+         * volumen
+         * volumenCilindro
+         * edadMinima / edadMin
+         * edad_minima
+         * e1a1_23
+         */
+        /*
+         * Tipos de dato
+         * Entero(int y long)
+         */
+        byte edad; // declara una variable de nombre edad y tipo de dato entero
+        edad = 20; // asignamos el valor de 20 a la variable edad
+        System.out.println(edad);
+
+        edad = 56;
+        System.out.println("y ahora es " + edad);
+
+        // Numeros decimales (doble y float)
+        double x, y;
+
+        x = 7;
+        y = 25.01;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        /*
+         * Cadena de catacteres (String)
+         */
+        String miPalabra = "Cerveza";
+        String miFrase = "¿Quién se tomo mi cerveza?";
+        System.out.println("Una palabra que uso frecuentemente es: " + miPalabra);
+        System.out.println("Una frase que uso a veces es: " + miFrase);
+
+        String cadenaInicial = "";
+
+        /*
+         * Caracteres (char)
+         */
+        char letra1 = 'c';
+        char letra2 = 'a';
+        char letra3 = 's';
+        char letra4 = 'a';
+
+        System.out.println(letra1+""+letra2+""+letra3+""+letra4);
+
+    }
+}
