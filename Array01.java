398c69f7dc6f01e935a27df61146f8a1d5a95530
Eduardo <echavez@utea.edu.pe>
2024-03-04 09:43:34 -0500

    arrays

---
+++ Array01.java
@@ -0,0 +1,26 @@
+public class Array01 {
+    public static void main(String[] args) {
+        // Como definimos un array
+        int[] arreglo;
+        double[] notas;
+        //espacios que va tener el arreglo
+        arreglo = new int[4];
+        notas = new double[5];
+        // llenar esos espacios
+        arreglo[0] = 12;
+        arreglo[1] = 15;
+        arreglo[2] = 17;
+        arreglo[3] = 11;
+        // Mostramos los valores que tiene el arreglo
+        System.out.println(arreglo[0]);
+        System.out.println(arreglo[1]);
+        System.out.println(arreglo[2]);
+        System.out.println(arreglo[3]);
+        int suma = arreglo[0]+arreglo[1];
+        System.out.println("Suma 1er con 2do: "+suma);
+
+
+        
+        
+    }
+}
