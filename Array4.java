fa0a8c8e11f6b609e4c65781bf3ab669b1f06443
Eduardo <echavez@utea.edu.pe>
2024-03-05 11:11:47 -0500

    Array4

---
+++ Array4.java
@@ -0,0 +1,34 @@
+public class Array4 {
+    public static void main(String[] args) {
+        /*Vamos a crear un programa que genere 100 numeros aleatorios del 0 al 20, los
+        mostraremos en pantalla separados por espacios, el programa pedirá que ingrese dos valores
+        por teclado y a continuacion el programa cambiara el primer por el segundo.
+         * 
+         */
+        int[] numeros = new int[100];
+
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = (int)(Math.random() * 21);
+            System.out.print(numeros[i]+ "-");
+        }
+
+        System.out.print("\nIngrese un número de los que se han mostrado: ");
+        int valor1 = Integer.parseInt(System.console().readLine());
+        System.out.print("Ingrese un valor para reemplazarlo: ");
+        int valor2 = Integer.parseInt(System.console().readLine());
+
+        String verde = "\033[32m";
+        String blanco = "\033[37m";
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] == valor1) {
+                numeros[i] = valor2;
+                System.out.print(verde + numeros[i]+"-");
+            }
+            else{
+                System.out.print(blanco + numeros[i]+"-");
+            }
+        }
+
+    }
+}
