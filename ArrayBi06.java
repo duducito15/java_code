4ca4ad193cb623146e46236f20be140cc1ae8862
Eduardo <echavez@utea.edu.pe>
2024-03-25 10:05:13 -0500

    Diagonal secundaria

--- ArrayBi06.java
+++ ArrayBi06.java
@@ -10,36 +10,46 @@ public class ArrayBi06 {
          */
         // 1. Declaramos nuestro array bidimencional
         int[][] matriz = new int[10][10];
-        int[] diagonal = new int[10];
+        int[] diagonalPrincipal = new int[10];
+        int[] diagonalSecundaria = new int[10];
         // 2. rellenar con numeros aleatorios
         for (int filas = 0; filas < 10; filas++) {
             for (int columnas = 0; columnas < 10; columnas++) {
                 matriz[filas][columnas] = (int) (Math.random() * 101) + 200;
                 //Determinando los elemntos de la diagonal
                 if (filas == columnas) {
-                    diagonal[filas] = matriz[filas][columnas];
+                    diagonalPrincipal[filas] = matriz[filas][columnas];
                 }
+                //Determinando los elementos de la diagonal secundaria
+                if (filas+columnas == 10 - 1) {
+                    diagonalSecundaria[filas] = matriz[filas][columnas];
+                }
+
                 System.out.printf("%5d", matriz[filas][columnas]);
             }
             System.out.println();
         }
+        System.out.println("Diagonal secundaria");
+        for (int i = 0; i < diagonalSecundaria.length; i++) {
+            System.out.printf("%5d", diagonalSecundaria[i]);
+        }
+
         int mayor = 200;
         int menor = 300;
         int suma = 0;
-        System.out.println("Diagonal principal");
-        for (int i = 0; i < diagonal.length; i++) {
-            System.out.printf("%5d", diagonal[i]);
-            if (diagonal[i] > mayor) {
-                mayor = diagonal[i];
+        System.out.println("\nDiagonal principal");
+        for (int i = 0; i < diagonalPrincipal.length; i++) {
+            System.out.printf("%5d", diagonalPrincipal[i]);
+            if (diagonalPrincipal[i] > mayor) {
+                mayor = diagonalPrincipal[i];
             }
-            if (diagonal[i] < menor) {
-                menor = diagonal[i];
+            if (diagonalPrincipal[i] < menor) {
+                menor = diagonalPrincipal[i];
             }
-            suma = suma + diagonal[i];
+            suma = suma + diagonalPrincipal[i];
         }
         System.out.println("\nMayor: "+mayor);
         System.out.println("Menor: "+menor);
         System.out.println("Promedio: "+((double)suma/10));
-
     }
 }
