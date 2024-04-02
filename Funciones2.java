502ce11bf05124455a8dc70baf33f69b471c2452
Eduardo <echavez@utea.edu.pe>
2024-04-02 11:23:58 -0500

    Ejercicios de funciones

---
+++ Funciones2.java
@@ -0,0 +1,23 @@
+public class Funciones2 {
+    public static void main(String[] args) {
+        //Ejercicios con funciones
+        /*
+         * 1. Hacer un programa que pase de binario a decimal y viceversa.
+         * 2. Amplie la funcionalidad del programa anterior de tal forma que permita
+         *      convertir un numero entre cualquiera de las siguietnes bases: decimal, binario,
+         *      hexadecimal y octal.
+         * 3. Crear una funcion que conveirte un numero n al sistema Morse.
+         *      tabla de quivalencias:
+         *      0	— — — — —
+         *      1	· — — — —
+         *      2	· · — — —
+         *      3	· · · — —
+         *      4	· · · · —
+         *      5	· · · · ·
+         *      6	— · · · ·
+         *      7	— — · · ·
+         *      8	— — — · ·
+         *      9	— — — — ·
+         */
+    }
+}
